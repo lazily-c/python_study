39c01d649b75b49cb71c3a14c035acea7dc8a545
seren~dipity <202597942@qq.com>
2023-06-14 19:12:24 +0800

    学习代码

---
+++ self_study/1/8.数据类型转换.py
@@ -0,0 +1,11 @@
+# bin() 用于将一个数据转化为二进制数据
+# oct() 用于将一个数据转化为八进制数据
+# int() 用于将一个数据转化为十进制数据
+# hex() 用于将一个数据转化为十六进制数据
+
+bin_num = 0b101  # 二进制数
+decimal = 10  # 十进制数
+print(oct(decimal))  # 将十进制数转化为八进制
+print(hex(decimal))  # 将十进制数转化为十六进制
+print(bin(decimal))  # 将十进制数转化为二进制
+print(int(bin_num))  # 将二进制数转化为十进制
