6db6ddb81ed31a15cc3f247010198aaff2fe9a89
lazily-c <2025973942@qq.com>
2023-07-23 21:09:01 +0800

    练习输入的数据类型转换

--- python_basics/2.输入输出&数据类型转换/格式化字符串.py
+++ python_basics/2.输入输出&数据类型转换/格式化字符串.py
@@ -1,32 +1,31 @@
-name_1 = "李元芳"
-age = 16
-height = 1.60
+# name_1 = "李元芳"
+# age = 16
+# height = 1.60
 
 # 要求：请以我叫李元芳，我的年龄是16，我的身高是1.9米，进行输出
 # 普通输出
-print(name_1, age, height)
+# print(name_1, age, height)
 
 # 格式化(方法一)，使用占位符，进行格式化
 # 使用格式符号(%s %d %f)
-print("我叫%s，我的年龄是%d，我的身高是%.2f米" % (name_1, age, height))
-print("我叫%s" % name_1)
+# print("我叫%s，我的年龄是%d，我的身高是%.2f米" % (name_1, age, height))
+# print("我叫%s" % name_1)
 
-print("---------------------------------")
 
 # 格式化(方法二)
 # 使用format()函数进行格式化
-str_1 = "黄河之水天上来"
-name = "李白"
-like = "作诗"
+# str_1 = "黄河之水天上来"
+# name = "李白"
+# like = "作诗"
 
 # python中一切皆对象
-print('我叫{},我喜欢{},有诗句{}'.format(name, like, str_1))
+# print('我叫{},我喜欢{},有诗句{}'.format(name, like, str_1))
 
-print("----------------------------------")
 
 # 格式化(方式三)
-# 在字符串前加f,如：f"xxx"{}"xxx"，大括号内传变量名
-print(f"我叫{name},我喜欢{like},有诗句{str_1}")
+# 在字符串前加f,如：f"xxx"{}"xxx"，
+# 大括号内传变量名，此方法是format的另一种形式。
+# print(f"我叫{name},我喜欢{like},有诗句{str_1}")
 
 # 换行
 print("天生我材必有用，\n千金散尽还复来")
@@ -36,7 +35,7 @@ print("天生我材必有用\t\t千金散尽还复来")
 # 输出\n
 print("i \\nlove python")
 
-# 原始字符串：去掉转义效果
+# 原始字符串：去掉转义效果,在字符串前加r
 print(r"i \nlove python")
 
 # 开发、研发
