4f056a0fee7faabf395f33fa705833daab251fc4
lazily-c <2025973942@qq.com>
2023-08-07 18:04:47 +0800

    删除代码

--- full_stack_study/1.开学典礼/1.基本操作.py
+++
@@ -1,7 +0,0 @@
-# 注释指的是不会参与程序的运行
-# 打印输出hello world这句话
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
-print("hello world")
--- full_stack_study/4.运算符/demo2.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-# 赋值运算符
-
-num = 10
-result1, result2, result3 = 10, 20, 30
-print(result1)
-print(result2)
-print(result3)
